bbed18a84e51a6bc190bf9146a4758952032743e
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-31 12:36:40 -0600

    Outlined initial psuedocode, and created user prompt = .

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+//A visitor receives a prompt upon opening the website
+const input = prompt("Please provide a list of flavors. Separate each on with a comma '',''")
+//to enter a list of comma-separated froyo flavors
+//The user types, "vanilla,vanilla,vanilla,strawberry,coffee,coffee"; should console.log vanilla = 3, strawberry = 1, coffee = 2;
+//
+
 const froyoList = [vanilla,strawberry,coffee,chocolate,mint,fudge];
