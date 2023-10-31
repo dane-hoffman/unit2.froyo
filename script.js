4f13a5988faafe12fcd51d97d20c7d183411c2d2
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-31 12:48:15 -0600

    Input propertly takes in values and displays as a list of strings to the console.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //A visitor receives a prompt upon opening the website
-const input = prompt("Please provide a list of flavors. Separate each on with a comma '',''")
+let input = prompt("Please provide a list of flavors. Separate each on with a comma '',''")
 //to enter a list of comma-separated froyo flavors
 //The user types, "vanilla,vanilla,vanilla,strawberry,coffee,coffee"; should console.log vanilla = 3, strawberry = 1, coffee = 2;
 //
 console.log(input);
+
+//The user's input striing is split into an array of strings.
+let onlineOrder = [];
+onlineOrder = input.split(",");
 //const froyoList = [vanilla,strawberry,coffee,chocolate,mint,fudge];
